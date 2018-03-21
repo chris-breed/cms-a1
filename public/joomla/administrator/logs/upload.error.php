8e3d6fe62860e4450034f87852e520e956d76b9d
chris-breed <christopher.breed@my.jcu.edu.au>
2018-03-21 21:47:52 +1000

    attemping to migrate local wp to host

---
+++ public/joomla/administrator/logs/upload.error.php
@@ -0,0 +1,15 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-03-21 11:16:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-03-21T11:16:44+00:00	INFO 192.168.33.1	upload	
+2018-03-21T11:16:52+00:00	INFO 192.168.33.1	upload	
+2018-03-21T11:17:08+00:00	INFO 192.168.33.1	upload	
+2018-03-21T11:17:19+00:00	INFO 192.168.33.1	upload	
+2018-03-21T11:17:23+00:00	INFO 192.168.33.1	upload	File exists: /var/www/public/joomla/images/001picture.png by user_id 827
+2018-03-21T11:17:31+00:00	INFO 192.168.33.1	upload	File exists: /var/www/public/joomla/images/001picture.png by user_id 827
+2018-03-21T11:17:52+00:00	INFO 192.168.33.1	upload	
+2018-03-21T11:17:57+00:00	INFO 192.168.33.1	upload	
+2018-03-21T11:18:51+00:00	INFO 192.168.33.1	upload	
--- public/joomla/media/template/jl_startup_free.php
+++ public/joomla/media/template/jl_startup_free.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "2a3f648ae21af3ba3dc3ef758358db55",
+    "tmpl_check": "72a4550907c58c3547f820eec04098f5",
     "tmpl_data": "<!DOCTYPE HTML PUBLIC \"-\/\/IETF\/\/DTD HTML 2.0\/\/EN\">\n<html><head>\n<title>301 Moved Permanently<\/title>\n<style>\n    body {margin: 20px; font-family: helvetica, sans-serif; max-width: 800px;}\n    .error {color: #e00;}\n    pre {font-size: 16px;}\n    h1 {font-size: 28px;}\n<\/style>\n<\/head><body>\n<h1>Moved Permanently<\/h1>\n<p>The document has moved <a href=\"https:\/\/yootheme.com\/updates?application=jl_startup_free_j25&amp;version=1.0.3&amp;format=raw\">here<\/a>.<\/p>\n<\/body><\/html>\n",
-    "warp_check": "2ff21ac3024ca36f59a35ec0c6b9da5a",
+    "warp_check": "6c81addca9c038d3a63ee8a8f41dd065",
     "warp_data": "<!DOCTYPE HTML PUBLIC \"-\/\/IETF\/\/DTD HTML 2.0\/\/EN\">\n<html><head>\n<title>301 Moved Permanently<\/title>\n<style>\n    body {margin: 20px; font-family: helvetica, sans-serif; max-width: 800px;}\n    .error {color: #e00;}\n    pre {font-size: 16px;}\n    h1 {font-size: 28px;}\n<\/style>\n<\/head><body>\n<h1>Moved Permanently<\/h1>\n<p>The document has moved <a href=\"https:\/\/yootheme.com\/updates?application=warp&amp;version=7.3.36&amp;format=raw\">here<\/a>.<\/p>\n<\/body><\/html>\n"
 }
